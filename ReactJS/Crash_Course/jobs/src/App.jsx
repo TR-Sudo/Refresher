13b2f98a622ce452792f34e3903634a2e739bb26
Talha Rashid <T.rashid489@gmail.com>
2024-11-27 20:07:11 -0500

    Link,Navlink,Error page

--- ReactJS/Crash_Course/jobs/src/App.jsx
+++ ReactJS/Crash_Course/jobs/src/App.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
-import {Route,createBrowserRouter,createRoutesFromElements,RouterProvider} from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/jobListings'
-import ViewAllJobs from './components/ViewAllJobs'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider} 
+  from 'react-router-dom'
+
+
 import HomePage from './pages/HomePage'
 import MainLayout from './layout/MainLayout'
+import JobsPage from './pages/JobsPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={<MainLayout/>}>
     <Route index element={<HomePage/>}/>
+    <Route path='/jobs' element={<JobsPage/>}/>
+    <Route path='*' element={<NotFoundPage/>}/>
   </Route>) // whatever is inside ths route will include that layout
+  // * is a catch all path error page shows the page not found element
 );
 
 function App() {
--- ReactJS/Crash_Course/jobs/src/components/JobListing.jsx
+++ ReactJS/Crash_Course/jobs/src/components/JobListing.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import {FaMapMarker} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 function JobListing({job}) {
   const [showFullDesc,setShowFullDesc] = useState(false) // dont show desc by default
@@ -38,12 +39,12 @@ function JobListing({job}) {
                   <FaMapMarker className='inline text-lg mb-3 mr-1 mt-2'></FaMapMarker>
                   {job.location}
                 </div>
-                <a
-                  href={`/job/${job.id}`}// grounds for routing
+                <Link
+                  to={`/job/${job.id}`}// grounds for routing
                   className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
                  Read More
-                </a>
+                </Link>
               </div>
             </div>
           </div>
