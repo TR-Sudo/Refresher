9e7248457fd0bed398f65d7e972fa8bb67870199
Talha Rashid <T.rashid489@gmail.com>
2024-12-04 01:12:52 -0500

    npm,npx,global,dev

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -1,4 +1,7 @@
 
+// npm -D saves as dev dep and you wont need these packages during prod
+// globally install node packages using -g [npm i -g nodemon]...
+// [ you wont need to type npm... package name but just type the package name]
 
 const _ = require('lodash')
 
@@ -7,3 +10,4 @@ const items = [1,[2,[3,[4]]]]
 const newItems = _.flattenDeep(items);
 
 console.log(newItems)
+console.log("Hello People")
