a1f2a61347d9486c1ee14ff6bebc75470b1ebb0a
Talha Rashid <T.rashid489@gmail.com>
2024-12-05 02:00:58 -0500

    express intro, http, styles, dependencies

--- ExpressJS/node-express-course-main/02-express-tutorial/app.js
+++ ExpressJS/node-express-course-main/02-express-tutorial/app.js
@@ -1,1 +1,45 @@
 console.log('Express Tutorial')
+const {readFileSync} = require('fs')
+
+//get all files
+const HomeP= readFileSync('./navbar-app/index.html')
+const HomeStyles= readFileSync('./navbar-app/styles.css')
+const HomeImage= readFileSync('./navbar-app/logo.svg')
+const HomeLogic= readFileSync('./navbar-app/browser-app.js')
+// you need to make a const for each style and requirement
+
+const http= require("http")
+
+const server=http.createServer((req,res)=>{
+    console.log(req.method,req.url)
+    if(req.url==='/'){
+        res.writeHead(200,{'content-type':'text/html'})// status code 200 ok, html type
+        res.write(HomeP)
+        res.end()//required
+    }
+    else if(req.url==='/about'){
+        res.writeHead(200,{'content-type':'text/html'})// status code 200 ok, html type
+        res.write('<h1>About Page</h1>')
+        res.end()//required
+    }else if(req.url==='/styles.css'){
+        res.writeHead(200,{'content-type':'text/css'})// status code 200 ok, html type
+        res.write(HomeStyles)
+        res.end()//required
+    }else if(req.url==='/logo.svg'){
+        res.writeHead(200,{'content-type':'image/svg+xml'})// status code 200 ok, html type
+        res.write(HomeImage)
+        res.end()//required
+    }else if(req.url==='/browser-app.js'){
+        res.writeHead(200,{'content-type':'text/javascript'})// status code 200 ok, html type
+        res.write(HomeLogic)
+        res.end()//required
+    }else{
+        res.writeHead(404,{'content-type':'text/html'})// status code 404 Not Found, html type
+        res.write('<h1>Not Found</h1>')
+        res.end()//required
+    }
+    console.log('user hit the server')
+    
+})
+
+server.listen(5000)
